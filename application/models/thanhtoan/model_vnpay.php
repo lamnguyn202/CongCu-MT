40409878e0bc605b9b99d1bc0befea89d1f8a8c0
lam <lamnguyn3112@gmail.com>
2024-04-15 22:58:45 +0700

    thanhtoan

---
+++ application/models/thanhtoan/model_vnpay.php
@@ -0,0 +1,32 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class model_vnpay extends CI_Model {
+
+	public $variable;
+
+	public function __construct()
+	{
+		parent::__construct();
+		
+	}
+
+	public function add($khachHangId, $madonhang, $sotien, $noidung, $nganhang, $magiaodich){
+		$sql = "INSERT INTO vnpay(khachHangId, madonhang, sotien, noidung, nganhang, magiaodich) VALUES (?,?,?,?,?,?)";
+		$result = $this->db->query($sql,array($khachHangId, $madonhang, $sotien, $noidung, $nganhang, $magiaodich));
+		return $result;
+	}
+	public function giaohang($khachHangId){
+		$sql = "SELECT * from vnpay where khachHangId=?";
+		$result = $this->db->query($sql, array($khachHangId));
+		return $result->result_array();
+	}
+	public function chitiethoadon($madonhang){
+		$sql = "SELECT sanpham.* , chitiethoadon.soLuong as 'soluongban', chitiethoadon.dagiaohang  as 'dagiaohang' FROM chitiethoadon, sanpham WHERE sanpham.sanPhamId = chitiethoadon.sanPhamId and madonhang = ?";
+		$result = $this->db->query($sql, array($madonhang));
+		return $result->result_array();
+	}
+	
+}
+
+/* End of file model_product.php */
+/* Location: ./application/models/model_product.php */
